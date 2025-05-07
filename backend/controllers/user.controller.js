a3fe6f47e383b162fc9eace1e98bebcd272c0c31
fasasimayokun <maykay.fash@gmail.com>
2025-05-07 08:11:04 +0100

    3- useFollow hook added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -55,7 +55,6 @@ export const followUnfollowUser = async (req, res) => {
 
             await newNotification.save();
 
-            // todo return id of the user as a response
             res.status(200).json({ message: "User followed successfully"});
         }
     } catch (error) {

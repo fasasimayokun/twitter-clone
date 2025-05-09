94b2570aadd7a084a39541994b3d4d51587efc80
fasasimayokun <maykay.fash@gmail.com>
2025-05-09 09:18:47 +0100

    4- prepared our app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -17,6 +18,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // middlewares
 app.use(express.json({limit:"5mb"})); // to parse req.body
@@ -27,6 +29,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get(/(.*)/, (req, res) => {
+        res.sendFile(path.resolve(__dirname,  "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
